3890b1ad7cdc4afb85b88c6491c007abe7e42517
Catherine <43898150+CatherineChernogor@users.noreply.github.com>
2020-02-18 10:51:45 +0800

    Updates

---
+++ precache-manifest.d6a1c6fa1e5ce247f220597986b54e54.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c15abef3954bf30ecff9d484608d5ea",
+    "url": "/alt_nti/index.html"
+  },
+  {
+    "revision": "e110e5b675f47aa1f91e",
+    "url": "/alt_nti/static/css/main.e095f10b.chunk.css"
+  },
+  {
+    "revision": "2687f6254bda6d4149d9",
+    "url": "/alt_nti/static/js/2.adbf15c6.chunk.js"
+  },
+  {
+    "revision": "94b03e19a4c9eb66db847f3cca1e03ba",
+    "url": "/alt_nti/static/js/2.adbf15c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e110e5b675f47aa1f91e",
+    "url": "/alt_nti/static/js/main.2d4017f0.chunk.js"
+  },
+  {
+    "revision": "2864f44c1282cc3bf4f5",
+    "url": "/alt_nti/static/js/runtime-main.6deee1e5.js"
+  },
+  {
+    "revision": "ba46c001740693f92b7f5558cfc1286b",
+    "url": "/alt_nti/static/media/down-arrow.ba46c001.svg"
+  },
+  {
+    "revision": "114b46f620ead013a812905719ec17e4",
+    "url": "/alt_nti/static/media/logo.114b46f6.png"
+  },
+  {
+    "revision": "e6956db22f6b55500046d41bb128484a",
+    "url": "/alt_nti/static/media/mail.e6956db2.svg"
+  },
+  {
+    "revision": "9a39f5536d9d9b9a2a2d8d86841f91f7",
+    "url": "/alt_nti/static/media/menu.9a39f553.svg"
+  },
+  {
+    "revision": "c28d9f7daba3e75dd87246748b01f102",
+    "url": "/alt_nti/static/media/up-arrow.c28d9f7d.svg"
+  }
+]);
