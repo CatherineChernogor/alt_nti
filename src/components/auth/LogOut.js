97cf6e9a770d73ac2db7dc274abb71b1146e803a
Catherine <43898150+CatherineChernogor@users.noreply.github.com>
2020-03-22 14:28:36 +0800

    add logout btn

---
+++ src/components/auth/LogOut.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import './auth.css';
+import { Redirect } from 'react-router-dom';
+
+const LogOutRender = (props) => {
+
+    const handler = () => {
+        sessionStorage.removeItem('token');
+        props.globalAction.setIsToken(false);
+    }
+    return (
+        <button className="logout-button" onClick={handler}>LogOut</button>
+    );
+}
+
+
+const LogOutHandler = (props) => {
+
+    useEffect(() => { }, [props.isToken]);
+
+    return (
+
+        props.isToken ?
+            <LogOutRender
+                isToken={props.isToken}
+                globalAction={props.globalAction} />
+            :
+            <Redirect to="/auth" />
+    )
+}
+export default LogOutHandler;
+
--- src/components/main/MainRender.js
+++ src/components/main/MainRender.js
@@ -6,6 +6,7 @@ import Chat from '../small/chat/ChatFun';
 import Points from '../small/points/Points';
 import Notification from '../small/NotificationFun';
 import RenderTasks from '../task/RenderTasks';
+import LogOut from '../auth/LogOut';
 
 const MainRender = (props) => {
 
@@ -14,10 +15,15 @@ const MainRender = (props) => {
             <NavMenu globalState={props.globalState} />
             <Chat globalState={props.globalState} />
             <Notification />
+            <LogOut
+                isToken={props.globalState.isToken}
+                globalAction={props.globalActions} />
             <div className="content main">
 
                 <Points globalState={props.globalState} />
-                <RenderTasks tasks={props.globalState.tasks} globalState={props.globalState} />
+                <RenderTasks
+                    tasks={props.globalState.tasks}
+                    globalState={props.globalState} />
             </div>
         </div>
     );
